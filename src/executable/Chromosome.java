2a7157e3feded5d8c903fa2e928822d6abc7fe4c
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2016-09-25 09:59:05 +0700

    Tambah git ignore untuk folder executable

---
+++ src/executable/Chromosome.java
@@ -0,0 +1,67 @@
+/**
+ * Definisi string untuk genetic algorithm
+ */
+
+public class Chromosome {
+	private Graph graph;
+	//Untuk caching, jadi ngk ngitung fitness berulang2
+	private int fitness = 0;
+
+	public Chromosome() {
+		//System.out.println("cctor 1");
+	}
+
+	public Chromosome(Graph graph) {
+		//System.out.println("cctor 2");
+		this.graph = graph;
+	}
+
+	public Chromosome(Chromosome cr) {
+		//System.out.println("cctor 3");
+		this.graph = cr.graph;
+	}
+
+	public Graph getGraph() {
+		return graph;
+	}
+
+	public void setGraph(Graph graph) {
+		this.graph = graph;
+	}
+
+	public void setGene(int idx, Node nd) {
+		graph.setGene(idx, nd);
+	}
+
+	public Node getGene(int idx) {
+		return graph.getGene(idx);
+	}
+
+	public int size() {
+		return graph.getVariables().length;
+	}
+
+	public int getFitness() {
+		/*
+		if (fitness==0) {
+			fitness = graph.getFitness();
+		}
+		return fitness;
+		*/
+		return graph.getFitness();
+	}
+
+	//Ini yang dimutasi satu aja
+	public void mutate() {
+		graph.mutate();
+	}
+
+	public void print() {
+		graph.print();
+	}
+
+	 public int getMaxFitness() {
+        return graph.getMaxFitness();
+    }
+
+}
---
+++ src/executable/Domain.java
@@ -0,0 +1,57 @@
+/**
+ * Domain of a CSP variable.
+ * Author: Garmastewira
+ * Date: 20 September 2016
+ */
+public class Domain {
+    private int day;
+    private int startTime;
+    //Kenapa dura
+    private int duration;
+    private String room;
+
+    public Domain(int day, int startTime, int duration, String room) {
+        this.day = day;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.room = room;
+    }
+
+    public Domain(Domain d) {
+        System.out.println("sampe sini");
+        System.out.println(d);
+        System.out.println("ngk sampe sini");
+        day = d.day;
+        startTime = d.startTime;
+        duration = d.duration;
+        room = d.room;
+    }
+
+    public static boolean isSame(Domain d1, Domain d2) {
+        return (d1.day==d2.day && d1.startTime==d2.startTime && d1.duration==d2.duration && d1.room==d2.room);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getFinishTime() {
+        return startTime + duration;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void print() {
+        System.out.println("Hari : " + day + ", jam mulai : " + startTime + ", durasi : " + duration + ", ruangan : " + room);
+    }
+}
